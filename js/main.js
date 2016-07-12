04fc580c2a558cbe540a3fbe1fde62dc68e039f2
SigridMonsalve <n7.monsalve@gmail.com>
2016-07-12 12:20:38 -0400

    Agregado reloj que se actualiza y display de ciudades -sin ciudades

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function visualizar(){
+	document.getElementById('displayOver').setAttribute('style',"visibility:visible;");
+	
+}
+function esconder(){
+	document.getElementById('displayOver').setAttribute('style',"visibility:hidden;");
+}
